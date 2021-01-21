3220ef222e26390587b5750ee26d9f0e4a2651a8
James Grieve <pocojames@hotmail.com>
2021-01-20 18:58:01 -0800

    add basic access to api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './main.scss';
+
+async function getApiData(city) {
+  const reponse = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b9b3ce70c93afe0a18d531d803314b8d`,
+    { mode: 'cors' }
+  );
+  const weatherData = await reponse.json();
+  console.log(weatherData);
+}
+
+getApiData('vancouver');
