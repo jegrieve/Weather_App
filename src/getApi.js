1775b5b8a3b3192998824a3a999373a23f536f1d
James Grieve <pocojames@hotmail.com>
2021-01-21 23:07:56 -0800

    made some bug fixes

--- src/getApi.js
+++ src/getApi.js
@@ -1,6 +1,6 @@
 async function getApiData(city) {
   const response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b9b3ce70c93afe0a18d531d803314b8d`,
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b9b3ce70c93afe0a18d531d803314b8d`,
     { mode: 'cors' }
   );
   const weatherData = await response.json();
--- src/index.js
+++ src/index.js
@@ -20,17 +20,3 @@ submitInputBtn.addEventListener('click', function (e) {
     changeBackgroundImg(d);
   });
 });
-
-//Want city and country
-//want temperature (F)
-//want weather ("clouds")
-//want feels like temp
-//want wind (5mph)
-//want humidity (69%)
-
-//So when I get in
-//1.get each of these data into variables and log them out
-//2.Work on the basic html portions
-//3.The screen will show each of these properties
-//4.Input search bar will work
-//5.add styling and any extra things necessary
